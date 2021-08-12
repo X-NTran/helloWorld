7697fc5c8e2c861ef83d22675f9e5e799452db78
xuan <xuannhutran@computingforall.org>
2021-08-12 14:25:15 -0700

    added a script on index.html'

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hello World!');
+// var, let, or const indicates a declaration
+// when you see '=' it indicates an assignment
+var firstName = "XuanNhu";
+let lastName = "Tran";
+// You cannot reassign constan variables!
+const age = 17;
+// Variables reassginments
+firstName = "My Dude";
+lastName = "Pinapple";
+
+console.log('Hello ' + firstName + ' ' + lastName + '!' + ' you are ' + age + ' years old!');
